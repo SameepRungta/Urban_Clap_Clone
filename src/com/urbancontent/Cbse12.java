5ed769e91e9fd057ac921cc9798f7b91540b8150
SameepRungta <sameeprungta2002@gmail.com>
2023-11-23 23:21:40 +0530

    Urban Clap Push

---
+++ src/com/urbancontent/Cbse12.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.urbancontent;
+
+/**
+ *
+ * @author SAM
+ */
+public class Cbse12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       wel ab= new wel();
+            ab.setVisible(true);
+            
+    }
+    
+}
